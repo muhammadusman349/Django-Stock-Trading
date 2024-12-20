c6128145419b13ac6f8230fe86e8fc1967b54c35
muhammad usman <usmankhadim.uk.uk@gmail.com>
2024-12-20 09:22:29 +0500

    stock sync with jupyter notebook

--- src/market/models.py
+++ src/market/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from timescale.db.models.fields import TimescaleDateTimeField
 from timescale.db.models.managers import TimescaleManager
+from . import tasks
 
 # Create your models here.
 
@@ -12,6 +13,11 @@ class Company(models.Model):
     active = models.BooleanField(default=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.ticker = f"{self.ticker}".upper()
+        super().save(*args, **kwargs)
+        tasks.sync_company_stock_quotes.delay(self.pk)
 
 
 class StockQuote(models.Model):
